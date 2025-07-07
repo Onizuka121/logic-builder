79f6e703ec580d3437a868916dfe649abf7a071f
Oni <walidfikry5@gmail.com>
2025-07-07 23:51:54 +0200

    map k

--- thrue-table-gen.py
+++ thrue-table-gen.py
@@ -2,6 +2,7 @@ import sympy
 from sympy.logic.boolalg import truth_table
 from texttable import Texttable
 from collections import deque
+import plotly.graph_objects as go
 
 
 exp_str = input("expression : ");
@@ -24,19 +25,24 @@ def get_gray_code(n):
             
             code[j] = list(d)
             j += 1
-    print(code) 
     return code
     
+
+def get_binary_conv(l1,l2=[]):
+    t = 0
+    c = len(l1)+len(l2)-1
+    for l in [l1,l2]:
+        for bit in l:
+            t += bit*(2**c)
+            c-=1
+
+    return t
+
 
 def get_modified_tb(table):
     nt = []
     for i in range(len(table)):
-        t = 0
-        c = len(table[i][0])-1
-        for bit in table[i][0]:
-            t += bit*(2**c)
-            c-=1
-
+        t = get_binary_conv(table[i][0])
         nt.append((t,table[i][1]))
 
     return nt
@@ -47,18 +53,42 @@ def get_k_map(vars,table):
     c = int(len(vars)/2)
     a = get_gray_code(c)
     b = get_gray_code(len(vars)-c)
+    table = get_modified_tb(table)
+    print("a: ",a)
+    print("b: " ,b)
     print(table)
-    print(get_modified_tb(table))
-
     map = []
     for ia in range(len(a)):
         map.append([])
         for ib in range(len(b)):
-            print(a[ia])
-    
-            #convert a[ia]+b[ib] to integer so its easy to map to table  
+            index = get_binary_conv(a[ia],b[ib])
+            print("info table : ",table[index],table[index][0], table[index][1])
+            map[ia].append(1 if table[index][1] else 0)
+    print_map(a,b,map)
 
+def print_map(a,b,map):
+    print(map)
+    b = [''.join(str(x) for x in col) for col in b]
+    a = [''.join(str(x) for x in row) for row in a]
 
+    fig = go.Figure(data=go.Heatmap(
+    z=map,
+    x=b,  
+    y=a,
+    colorscale=[[0, 'white'], [1, 'black']],
+    hoverongaps=False,
+    showscale=False,
+    zmax=1,
+    zmin=0
+))
+
+    fig.update_layout(
+    title='Mappa di Karnaugh 2×4',
+    xaxis_title='Variabili BC',
+    yaxis_title='Variabile A'
+)
+
+    fig.show()
 
 def get_tb_from_fromula(formula):
     try:
